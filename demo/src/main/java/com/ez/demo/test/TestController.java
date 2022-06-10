6b222a3b96e74b2bc8ad3762464127b2687973eb
hyunsooyk <107205330+hyunsooyk@users.noreply.github.com>
2022-06-10 14:19:00 +0900

    TestController 수정 by hong

--- demo/src/main/java/com/ez/demo/test/TestController.java
+++ demo/src/main/java/com/ez/demo/test/TestController.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Controller;
 public class TestController {
 
 	public String test() {
-		System.out.println("test by hong");
-		
+		System.out.println("test by hong - TestController");
+		System.out.println("hong!!!!");
 		return "/index";
 	}
 }
